656f043270ec32c81c5088b0a1244ab36533c4ea
Carlos Queiroz <cqueiroz@gopivotal.com>
2015-11-24 13:13:53 +0800

    Adds maven wrappers.

--- src/main/java/io/pivotal/springtrader/SpringTraderAppMonolith.java
+++ src/main/java/io/pivotal/springtrader/SpringTraderAppMonolith.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringTraderAppMonolithApplication {
+public class SpringTraderAppMonolith {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringTraderAppMonolithApplication.class, args);
+        SpringApplication.run(SpringTraderAppMonolith.class, args);
     }
 }
