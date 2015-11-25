5fad5b42042e70fe3824c0ec7f79626a79142c3e
Carlos Queiroz <caxqueiroz@gmail.com>
2015-11-25 17:36:46 +0800

    Disables GET for /login

--- src/main/java/io/pivotal/springtrader/controllers/AuthenticationController.java
+++ src/main/java/io/pivotal/springtrader/controllers/AuthenticationController.java
@@ -7,12 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-
-//import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
  * REST controller for the io.pivotal.springtrader.accounts.accounts microservice.
@@ -22,6 +19,7 @@ import java.util.Map;
  * <li>GET <code>/logout/{userId}</code> logs out the account with given user id.
  * </ul><p>
  * @author David Ferreira Pinto
+ * @author cq
  *
  */
 @RestController
@@ -62,14 +60,7 @@ public class AuthenticationController {
 		accountService.logout(userId);
 	}
 
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String getLogin(Model model, @ModelAttribute(value="login") AuthenticationRequest login) {
-		logger.info("Logging in GET, user: " + login.getUsername());
-		if(login.getUsername()==null) {
-            if (!model.containsAttribute("login")) {
-                model.addAttribute("login", new AuthenticationRequest());
-            }
-        }
-		return "index";
-	}
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @ResponseStatus( HttpStatus.METHOD_NOT_ALLOWED )
+    public void get() {}
 }
